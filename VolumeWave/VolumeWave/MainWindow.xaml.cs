b20e8fe11aed5a7044b2fe66a6f6d2576e5a9672
Techrocket9 <Techrocket9+gethub@gmail.com>
2013-09-01 18:11:24 -0700

    Initial Code Commit

---
+++ VolumeWave/VolumeWave/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CoreAudioApi;
+using System.Diagnostics;
+
+namespace VolumeWave
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MMDevice device;
+        private float periodAdjust;
+        private double start;
+        private float initialAudioLevel;
+        Thread runner;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+
+            runner = new Thread(()=>
+            {
+                MMDeviceEnumerator DevEnum = new MMDeviceEnumerator();
+                device = DevEnum.GetDefaultAudioEndpoint(EDataFlow.eRender, ERole.eMultimedia);
+                start = GetMSTime();
+                initialAudioLevel = device.AudioEndpointVolume.MasterVolumeLevelScalar;
+
+                while (true)
+                {
+                    double elapsed = (GetMSTime() - start)/1000;
+                    double temporalVolume = (1.3+Math.Sin(elapsed*periodAdjust))/2.6;
+
+
+                    device.AudioEndpointVolume.MasterVolumeLevelScalar = ((float)temporalVolume);
+                    Debug.WriteLine("Volume was set to: " + ((float)temporalVolume));
+                }
+            });
+
+            runner.Start();
+
+           
+
+        }
+
+        private static double GetMSTime()
+        {
+            TimeSpan t = (DateTime.UtcNow - new DateTime(1970, 1, 1));
+            double start = t.TotalMilliseconds;
+            return start;
+        }
+
+        private void setnewperiod(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            periodAdjust = (float)(e.NewValue*1.5);
+            //Debug.WriteLine("Set adjust to " + periodAdjust);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (start > 0) start += 10;
+        }
+
+        private void Audio_Cycle_Closed(object sender, EventArgs e)
+        {
+            runner.Abort();
+            device.AudioEndpointVolume.MasterVolumeLevelScalar = initialAudioLevel;
+        }
+    }
+}
